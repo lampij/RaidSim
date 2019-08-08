0002ff0b84714d84d6c23042cc552fa2095a4632
Benjamin Pinter <ben.david.pinter@gmail.com>
2019-08-08 18:59:28 -0400

    The first working execution of the application

---
+++ ContainerConfig.cs
@@ -0,0 +1,24 @@
+﻿using RaidSim.Application.Routines;
+using RaidSim.Interface.Routines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+using Unity.Lifetime;
+
+namespace RaidSim
+{
+    public class ContainerConfig
+    {
+        public static UnityContainer RegisterDependencies()
+        {
+            UnityContainer container = new UnityContainer();
+
+            container.RegisterType<IGameStart, GameStart>(new HierarchicalLifetimeManager());
+
+            return container;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using RaidSim.Interface.Routines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace RaidSim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _ = ContainerConfig.RegisterDependencies().Resolve<IGameStart>().StartGame();
+        }
+    }
+}
