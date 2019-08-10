dab5cd6435f250aea25b74c8fa73cef03948f0a6
Benjamin Pinter <ben.david.pinter@gmail.com>
2019-08-10 09:21:10 -0400

    Added logging packages.

--- RaidSim.Application/Routines/GameStart.cs
+++ RaidSim.Application/Routines/GameStart.cs
@@ -12,6 +12,7 @@ namespace RaidSim.Application.Routines
 {
     public class GameStart : IGameStart
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(GameStart));
         IPlayerFactory _playerFactory;
 
         public GameStart(IPlayerFactory playerFactory)
@@ -22,8 +23,12 @@ namespace RaidSim.Application.Routines
         public async Task StartGame()
         {
             Player[] players = await _playerFactory.CreateRandomPlayer(5);
+
+#if DEBUG
+            ///<remarks>This is only here so that I can debug the object array I'm working with.</remarks>
             Console.Write($"{JsonConvert.SerializeObject(players, Formatting.Indented)}");
             Console.ReadKey();
+#endif
         }
     }
 }
